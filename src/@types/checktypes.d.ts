29be1fd42814b92042bc74eb681ee68cca445058
islam yamor <islamyamor@pm.me>
2023-04-04 14:02:58 +0300

    initial commit

---
+++ src/@types/checktypes.d.ts
@@ -0,0 +1,5 @@
+declare module '@samislam/checktypes' {
+  export function isArray(arg: any): boolean
+  export function isAsycOrSyncFunc(arg: any): boolean
+  export function isObject(arg: any): boolean
+}
--- src/useMakeDispatchable.ts
+++ src/useMakeDispatchable.ts
@@ -1,3 +1,23 @@
-import React from 'react';
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { isObject, isAsycOrSyncFunc, isArray } from '@samislam/checktypes'
 
-export default function useMakeDispatchable() {}
+export default function useMakeDispatchable(reducers: any[] | object | (() => void)) {
+  const dispatch = useDispatch()
+  switch (true) {
+    case isObject(reducers): {
+      const DispatchableList: any = {}
+      for (const [key, value] of Object.entries(reducers)) {
+        DispatchableList[key] = (arg: any) => dispatch(value(arg))
+      }
+      return DispatchableList
+    }
+    case isAsycOrSyncFunc(reducers): {
+      return (arg: any) => dispatch((reducers as (arg: any) => void)(arg))
+    }
+    case isArray(reducers): {
+      const DispatchableList = (reducers as any[]).map((reducer) => (arg: any) => dispatch(reducer(arg)))
+      return DispatchableList
+    }
+  }
+}
